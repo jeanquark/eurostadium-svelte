49ee0a16e3cb519f31a2a760759bdac01f3e2426
jeanquark <jm.kleger@gmail.com>
2024-09-15 01:20:37 +0200

    Remove default init project

--- src/lib/firebase/firebase.js
+++ src/lib/firebase/firebase.js
@@ -7,12 +7,18 @@ import { getAuth } from 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID
+  // apiKey: import.meta.env.VITE_API_KEY,
+  apiKey: 'AIzaSyDyVXQ8WotawTgnhsqtcIplCeThjbVIpc4',
+  // authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  authDomain: 'eurostadium-895f4.firebaseapp.com',
+  // projectId: import.meta.env.VITE_PROJECT_ID,
+  projectId: 'eurostadium-895f4',
+  // storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  storageBucket: 'eurostadium-895f4.appspot.com',
+  // messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  messagingSenderId: 1059997861153,
+  // appId: import.meta.env.VITE_APP_ID
+  appId: '1:1059997861153:web:5d98d07ca2367bc215aa60'
 };
 
 // Initialize Firebase
