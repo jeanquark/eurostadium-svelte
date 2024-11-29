44408a12145bc4000ca00ef19a408cba0b32b4a2
Jean-Marc <jm.kleger@gmail.com>
2024-11-29 01:45:46 +0100

    Update camelize function

--- src/lib/utils/convertToCamelCase.js
+++ src/lib/utils/convertToCamelCase.js
@@ -1,7 +1,9 @@
 export default function camelize(str) {
-    return str
-        .replace(/(?:^\w|[A-Z]|\b\w)/g, function (word, index) {
-            return index === 0 ? word.toUpperCase() : word.toUpperCase()
-        })
-        .replace(/\s+/g, '')
+    // return str
+    //     .replace(/(?:^\w|[A-Z]|\b\w)/g, function (word, index) {
+    //         return index === 0 ? word.toUpperCase() : word.toUpperCase()
+    //     })
+    //     .replace(/\s+/g, '')
+
+    return str.charAt(0).toUpperCase() + str.replace(/_./g, str=>str[1].toUpperCase()).slice(1)
 }
